c20e2036b510f163c36518c297068e7d3f411909
呆瓜云 <meekdai@163.com>
2023-07-04 10:45:39 +0800

    初始化2

--- blog.py
+++ blog.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# https://pygithub.readthedocs.io/en/stable/examples/Issue.html
-from github import Github
-g = Github("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")
-repo=g.get_user().get_repos()[10]
-
 import markdown
+import argparse
+from github import Github
 from xpinyin import Pinyin
 py=Pinyin()
+
+# g = Github("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")
+# repo=g.get_user().get_repos()[10]
+
+
+
 
 def md2html(title,mdstr):
     exts = ['markdown.extensions.extra', 'markdown.extensions.codehilite','markdown.extensions.toc']
@@ -28,16 +29,22 @@ def md2html(title,mdstr):
     ret = markdown.markdown(mdstr,extensions=exts)
     return html % (title,ret)
 
-def saveHtml(title,body):
-    genHtml = 'docs/post/{}.html'.format(py.get_pinyin(title))
+def saveHtml(title,body,dir_name):
+    genHtml = dir_name+'/post/{}.html'.format(py.get_pinyin(title))
     f = open(genHtml, 'w', encoding='UTF-8')
     message = md2html(title,body)
     f.write(message)
     f.close()
 
-def savePost(id):
-    saveHtml(repo.get_issues()[id].title,repo.get_issues()[id].body)
+def main(token,repo_name,issue_number=None, dir_name="docs"):
+    user = Github(token)
+    repo= user.get_user().get_repo(repo_name)
+    for issue in repo.get_issues():
+        saveHtml(issue.title,issue.body,dir_name)
 
-savePost(0)
-savePost(1)
-savePost(2)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("github_token", help="github_token")
+    parser.add_argument("repo_name", help="repo_name")
+    options = parser.parse_args()
+    main(options.github_token, options.repo_name)
