520b36ce5a8ac017a62b9ec0fbb5f7aa5c59a3f7
呆瓜云 <meekdai@163.com>
2023-07-11 17:52:23 +0800

    dark and light

--- meekblog.py
+++ meekblog.py
@@ -4,11 +4,9 @@ import requests
 import argparse
 from github import Github
 from xpinyin import Pinyin
-
 ######################################################################################
-
 class MEEKBLOG():
-    def __init__(self,options,post_dir):
+    def __init__(self,options,post_dir,backupMd=False):
         self.options=options
         self.post_dir=post_dir
         self.index_example=open('index_example.html', 'r', encoding='utf-8').read()
@@ -16,6 +14,7 @@ class MEEKBLOG():
         self.index_md=''
         self.post_year=0
         self.header_right=''
+        self.backupMd=backupMd
 
         user = Github(self.options.github_token)
         self.repo = self.get_repo(user, options.repo_name)
@@ -28,6 +27,9 @@ class MEEKBLOG():
         os.chdir("docs/")
         if not os.path.exists(self.post_dir):
             os.mkdir(self.post_dir)
+        if self.backupMd:
+            if not os.path.exists("backup/"):
+                os.mkdir("backup/")
 
     def get_repo(self,user: Github, repo: str):
         return user.get_repo(repo)
@@ -49,9 +51,11 @@ class MEEKBLOG():
         post_body=self.markdown2html(issue.body)
         f.write(self.value2postHtml(self.blog_name,issue.title,post_body,self.avatar_url,self.header_right))
         f.close()
-        # f = open("backup/"+issue.title+".md", 'w', encoding='UTF-8')
-        # f.write(issue.body)
-        # f.close()
+
+        if self.backupMd:
+            f = open("backup/"+issue.title+".md", 'w', encoding='UTF-8')
+            f.write(issue.body)
+            f.close()
         return gen_Html
 
     def value2postHtml(self,blog_name,post_title,post_body,avatar_url,header_right):
@@ -83,7 +87,7 @@ class MEEKBLOG():
                         self.post_year=issue.created_at.year
                         self.index_md=self.index_md+("## %s \r\n"%self.post_year)
                     self.index_md=self.index_md+("- %s &nbsp;&nbsp;&nbsp;&nbsp;[%s](%s)\r\n" % (issue.created_at.strftime("%Y-%m-%d"),issue.title,gen_Html))
-                    print("create postPage title=%s file=%s ok" % (issue.title,gen_Html))
+                    print("create postPage title=%s file=%s " % (issue.title,gen_Html))
 
                 else:
                     gen_Html=self.createPostHtml(issue,single=label.name)
@@ -92,7 +96,7 @@ class MEEKBLOG():
                     
 
         self.creatIndexHtml()
-        print("create index.html ok")
+        print("create index.html")
         print("====== create static html end ======")
 
 ######################################################################################
