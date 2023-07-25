63a6a0f89473fc69a8c31fe6f7ea0ec8b089977f
呆瓜云 <meekdai@163.com>
2023-07-25 16:53:37 +0800

    new postList sorted

--- meekblog.py
+++ meekblog.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
+import json
+import time
+import datetime
+import shutil
 import requests
 import argparse
 from github import Github
 from xpinyin import Pinyin
 ######################################################################################
+avatar_url="http://meekdai.com/avatar.jpg"
+
+######################################################################################
 class MEEKBLOG():
-    def __init__(self,options,post_dir,backupMd=False):
+    def __init__(self,options,post_dir):
         self.options=options
         self.post_dir=post_dir
         self.index_example=open('index_example.html', 'r', encoding='utf-8').read()
@@ -14,22 +21,30 @@ class MEEKBLOG():
         self.index_md=''
         self.post_year=0
         self.header_right=''
-        self.backupMd=backupMd
+
+        self.postDict=json.loads('{}')
 
         user = Github(self.options.github_token)
         self.repo = self.get_repo(user, options.repo_name)
-        self.avatar_url=user.get_user().avatar_url
+        # self.avatar_url=user.get_user().avatar_url
+        self.avatar_url=avatar_url
         self.blog_name=user.get_user().login
 
-        self.checkPostDir()
+    #     self.checkPostDir()
 
-    def checkPostDir(self):
-        os.chdir("docs/")
-        if not os.path.exists(self.post_dir):
-            os.mkdir(self.post_dir)
-        if self.backupMd:
-            if not os.path.exists("backup/"):
-                os.mkdir("backup/")
+    # def checkPostDir(self):
+    #     if not os.path.exists(self.post_dir):
+    #         os.mkdir(self.post_dir)
+    #     if not os.path.exists("backup/"):
+    #         os.mkdir("backup/")
+
+    def cleanFile(self):
+        shutil.rmtree("backup/")
+        shutil.rmtree("docs/")
+
+        os.mkdir("backup/")
+        os.mkdir("docs/")
+        os.mkdir(self.post_dir)
 
     def get_repo(self,user: Github, repo: str):
         return user.get_repo(repo)
@@ -44,60 +59,88 @@ class MEEKBLOG():
 
     def createPostHtml(self,issue,single=None):
         if single==None:
-            gen_Html = self.post_dir+'{}.html'.format(Pinyin().get_pinyin(issue.title))
+            gen_Html = self.post_dir+'{}.html'.format(Pinyin().get_pinyin(issue["title"]))
         else:
-            gen_Html = single+".html"
-        f = open(gen_Html, 'w', encoding='UTF-8')
-        post_body=self.markdown2html(issue.body)
-        source_url="https://github.com/"+options.repo_name+"/issues/"+str(issue.number)
-        f.write(self.value2postHtml(self.blog_name,issue.title,post_body,self.avatar_url,source_url,self.header_right))
+            gen_Html = "docs/"+single+".html"
+        
+        f = open("backup/"+issue["title"]+".md", 'r', encoding='UTF-8')
+        post_body=self.markdown2html(f.read())
         f.close()
 
-        if self.backupMd:
-            f = open("backup/"+issue.title+".md", 'w', encoding='UTF-8')
-            f.write(issue.body)
-            f.close()
+        f = open(gen_Html, 'w', encoding='UTF-8')
+        f.write(self.value2postHtml(self.blog_name,issue["title"],post_body,self.avatar_url,issue["source_url"],self.header_right))
+        f.close()
+
         return gen_Html
 
     def value2postHtml(self,blog_name,post_title,post_body,avatar_url,source_url,header_right):
         return self.post_example%(post_title,avatar_url,blog_name,header_right,post_title,source_url,post_body)
 
     def creatIndexHtml(self):
-        f = open("index.html", 'w', encoding='UTF-8')
+        self.postDict=dict(sorted(self.postDict.items(),key=lambda x:x[1]["created_at"],reverse=True))#使列表由时间排序
+        for num in self.postDict:
+            if self.postDict[num]["label"]=='post':
+                post_time = datetime.datetime.fromtimestamp(self.postDict[num]["created_at"])
+                if self.post_year!=post_time.year:
+                    self.post_year=post_time.year
+                    self.index_md=self.index_md+("## %s \r\n"%(self.post_year))
+                self.post_url=self.post_dir[5:]+'{}.html'.format(Pinyin().get_pinyin(self.postDict[num]["title"]))
+                self.index_md=self.index_md+("- %s &nbsp;&nbsp;&nbsp;&nbsp;[%s](%s)\r\n" % (post_time.strftime("%Y-%m-%d"),self.postDict[num]["title"],self.post_url))
+            else:
+                self.header_right=self.header_right+('<div class="Header-item"><a href="/%s.html" class="Header-link">%s</a></div>' % (self.postDict[num]["label"],self.postDict[num]["title"]))
+
+        f = open("docs/index.html", 'w', encoding='UTF-8')
         index_body=self.markdown2html(self.index_md)
         f.write(self.value2indexHtml(self.blog_name,index_body,self.avatar_url,self.header_right))
         f.close()
+        print("create docs/index.html")
 
     def value2indexHtml(self,blog_name,index_body,avatar_url,header_right):
         return self.index_example%(blog_name,avatar_url,blog_name,header_right,index_body)
 
+    def addOnePostJson(self,issue):
+        for label in issue.labels:
+            self.postDict[issue.number]=json.loads('{}')
+            self.postDict[issue.number]["label"]=label.name
+            self.postDict[issue.number]["title"]=issue.title
+            self.postDict[issue.number]["source_url"]="https://github.com/"+options.repo_name+"/issues/"+str(issue.number)
+            try:
+                modifyTime=json.loads(issue.body.split("\r\n")[-1:][0].split("##")[1])
+                self.postDict[issue.number]["created_at"]=modifyTime["timestamp"]
+            except:
+                self.postDict[issue.number]["created_at"]=int(time.mktime(issue.created_at.timetuple()))
+        f = open("backup/"+issue.title+".md", 'w', encoding='UTF-8')
+        f.write(issue.body)
+        f.close()
+
+    def creatOneHtml(self,issue):
+        if issue["label"]=='post':
+            gen_Html=self.createPostHtml(issue)
+            print("create postPage title=%s file=%s " % (issue["title"],gen_Html))
+        else:
+            gen_Html=self.createPostHtml(issue,single=issue["label"])
+            print("create singlePage title=%s file=%s ok" % (issue["title"],gen_Html))
+
     def runAll(self):
         print("====== start create static html ======")
+        self.cleanFile()
+
         issues=self.repo.get_issues()
-
         for issue in issues:
-            for label in issue.labels:
-                if label.name!='post':
-                    self.header_right=self.header_right+('<div class="Header-item"><a href="/%s.html" class="Header-link">%s</a></div>' % (label.name,issue.title))
+            self.addOnePostJson(issue)
 
-        for issue in issues:
-            for label in issue.labels:
-                if label.name=='post':
-                    gen_Html=self.createPostHtml(issue)
-                    if self.post_year!=issue.created_at.year:
-                        self.post_year=issue.created_at.year
-                        self.index_md=self.index_md+("## %s \r\n"%self.post_year)
-                    self.index_md=self.index_md+("- %s &nbsp;&nbsp;&nbsp;&nbsp;[%s](%s)\r\n" % (issue.created_at.strftime("%Y-%m-%d"),issue.title,gen_Html))
-                    print("create postPage title=%s file=%s " % (issue.title,gen_Html))
-
-                else:
-                    gen_Html=self.createPostHtml(issue,single=label.name)
-                    print("create singlePage title=%s file=%s ok" % (issue.title,gen_Html))
-                    
-                    
+        for num in self.postDict:
+            self.creatOneHtml(self.postDict[num])
 
         self.creatIndexHtml()
-        print("create index.html")
+        print("====== create static html end ======")
+
+    def runOne(self,number):
+        print("====== start create static html ======")
+        issues=self.repo.get_issues()
+        self.addOnePostJson(issues[int(number)])
+        self.creatOneHtml(self.postDict[number])
+        self.creatIndexHtml()
         print("====== create static html end ======")
 
 ######################################################################################
@@ -107,7 +150,26 @@ parser.add_argument("github_token", help="github_token")
 parser.add_argument("repo_name", help="repo_name")
 parser.add_argument("--issue_number", help="issue_number", default=0, required=False)
 options = parser.parse_args()
-blog=MEEKBLOG(options,'post/')
-blog.runAll()
+
+blog=MEEKBLOG(options,'docs/post/')
+
+if not os.path.exists("postList.json"):
+    print("postList is not exists, runAll")
+    blog.runAll()
+else:
+    if options.issue_number=="0":
+        print("issue_number=='0', runAll")
+        blog.runAll()
+    else:
+        f=open("postList.json","r")
+        print("postList is exists and issue_number!=0, runOne")
+        blog.postDict=json.loads(f.read())
+        f.close()
+        blog.runOne(options.issue_number)
+
+listFile=open("postList.json","w")
+listFile.write(json.dumps(blog.postDict))
+listFile.close()
+
 
 ######################################################################################
