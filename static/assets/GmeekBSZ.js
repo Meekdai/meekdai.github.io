cb1ab2dd03771507450c96a71f01c4105b17269e
Meekdai <meekdai@163.com>
2024-07-19 14:20:23 +0800

    Update GmeekBSZ.js

--- static/assets/GmeekBSZ.js
+++ static/assets/GmeekBSZ.js
@@ -17,26 +17,16 @@ function loadResource(type, attributes, callback) {
 }
 
 function createBSZ() {
-
     var postBody = document.getElementById('postBody');
-    var parent = postBody.parentNode;
-    var bszElement = document.createElement('div');
-    bszElement.id = 'busuanzi';
-    bszElement.innerHTML = "本文浏览量 ";
-    bszElement.style.display='none';
-    bszElement.style.float="left";
-    bszElement.style.marginTop="8px";
-    bszElement.style.fontSize="small";
-
-    var sitePV = document.createElement('span');
-    sitePV.id = 'busuanzi_value_page_pv';
-
-    bszElement.appendChild(sitePV);
-    parent.insertBefore(bszElement, postBody.nextSibling);
+    if (postBody){
+        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量 <span id="busuanzi_value_page_pv"></span>次</div>');
+    }
+    var runday = document.getElementById('runday');
+    runday.insertAdjacentHTML('afterend', '<div id="busuanzi_container_site_pv" style="display:none;">总浏览量 <span id="busuanzi_value_site_pv"></span>次 • </div>');
 }
-
 
 document.addEventListener("DOMContentLoaded", function() {
     createBSZ();
     loadResource('script', { src: '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js' });
+    console.log("\n %c GmeekBSZ Plugins https://github.com/Meekdai/Gmeek \n\n","padding:5px 0;background:#bc4c00;color:#fff");
 });
