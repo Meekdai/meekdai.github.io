5b298b23d5108798e4635f5d7555cd4ee572336d
Meekdai <meekdai@163.com>
2024-07-19 13:18:05 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Meekdai :link: https://blog.meekdai.com 
 ### :page_facing_up: [52](https://blog.meekdai.com/tag.html) 
-### :speech_balloon: 177 
+### :speech_balloon: 186 
 ### :hibiscus: 121398 
-### :alarm_clock: 2024-07-19 16:25:14 
+### :alarm_clock: 2024-07-19 21:18:05 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/assets/GmeekBSZ.js
+++ docs/assets/GmeekBSZ.js
@@ -10,7 +10,7 @@ function createBSZ() {
 document.addEventListener("DOMContentLoaded", function() {
     createBSZ();
     var element = document.createElement('script');
-    element.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js';
+    element.src = 'https://vercount.one/js';
     document.head.appendChild(element);
     console.log("\n %c GmeekBSZ Plugins https://github.com/Meekdai/Gmeek \n","padding:5px 0;background:#bc4c00;color:#fff");
 });
